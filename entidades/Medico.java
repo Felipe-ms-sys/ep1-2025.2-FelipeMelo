c2f7846af4d25e5edea3197c0edba19fd727a254
Felipe Melo <felipems.ctt@gmail.com>
2025-09-21 03:24:17 -0300

    Criação da classe Médico

---
+++ entidades/Medico.java
@@ -0,0 +1,33 @@
+package Projeto1Poo.entidades;
+
+public class Medico extends Pessoa{
+
+    private String crm;
+    private String especialidade;
+    
+    public Medico(String nome, String cpf, String endereço, String email, int idade, String telefone, char sexoBiologico, String crm, String especialidade){
+        super(nome, cpf, endereço, email, idade, telefone, sexoBiologico);
+        this.crm = crm;
+        this.especialidade = especialidade;
+    }
+
+    public String getCrm(){
+        return crm;
+    }
+
+    public void setCrm(String crm){
+        this.crm = crm;
+    }
+    
+    public String getEspecialidade(){
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade){
+        this.especialidade = especialidade;
+    }
+
+    
+}
+    
+
