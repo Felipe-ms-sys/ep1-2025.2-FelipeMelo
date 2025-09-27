2cd38ddf861dd5d0a2318a93993858e19ffe20b9
Felipe Melo <felipems.ctt@gmail.com>
2025-09-26 21:48:13 -0300

    Criação de um método de cadastro de pacientes

--- sistema/Main.java
+++ sistema/Main.java
@@ -1,6 +1,7 @@
 package Projeto1Poo.sistema;
+
 import Projeto1Poo.entidades.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main{
     
@@ -57,7 +58,8 @@ public class Main{
                         case 0:
                             estadoAtual = EstadoMenu.PRINCIPAL;
                             break;
-                        case 1: // Método de  cadastrar paciente
+                        case 1: 
+                            cadastrarPaciente();
                             break;
                         case 2: // Método de alterar dados do paciente
                             break;
@@ -107,10 +109,15 @@ public class Main{
                             estadoAtual = EstadoMenu.PRINCIPAL;
                             break;
                         case 1: // Método de agendar nova consulta
+                            break;
                         case 2: // Método de cancelar consulta
+                            break;
                         case 3: // Método de conferir agendamentos
+                            break;
                         case 4: // Método de registrar diagnóstico
+                            break;
                         case 5: // Método de conferir dados da consulta
+                            break;
                     }
                     break;
 
@@ -124,10 +131,15 @@ public class Main{
                             estadoAtual = EstadoMenu.PRINCIPAL;
                             break;
                         case 1: // Método de registrar nova internação
+                            break;
                         case 2: // Método de cancelar internação
+                            break;
                         case 3: // Método de registrar alta de paciente
+                            break;
                         case 4: // Método de consultar internações
+                            break;
                         case 5: // Método de histórico de internações
+                            break;
                     }
                     break;
 
@@ -141,10 +153,14 @@ public class Main{
                             estadoAtual = EstadoMenu.PRINCIPAL;
                             break;
                         case 1: // Método de relatório dos pacientes
+                            break;
                         case 2: // Método de relatório dos médicos
+                            break;
                         case 3: // Método de relatório das consultas
+                            break;
                         case 4: // Método de pacientes internados
-                    }
+                            break;
+                    }       
                     break;
 
                 default: 
@@ -152,5 +168,109 @@ public class Main{
                     break;
             }
         }
+
+        }
+
+        private static void cadastrarPaciente(){
+            String status = "Ativo";
+            String numeroProntuario = "P" + (Paciente.listarTodos().size() + 1);
+
+            System.out.println("-------- Cadastrar Novo Paciente -------");
+
+            System.out.print("Nome: ");
+            String nome = scanner.nextLine();
+
+            System.out.print("CPF: ");
+            String cpf = scanner.nextLine();
+
+            System.out.print("Email: ");
+            String email = scanner.nextLine();
+
+            System.out.print("Telefone: ");
+            String telefone = scanner.nextLine();
+
+            System.out.print("Idade: ");
+            int idade = scanner.nextInt();
+            scanner.nextLine(); 
+
+            System.out.print("Sexo Biológico (M/F): ");
+            String sexoBiologico = scanner.nextLine().toUpperCase();
+
+            System.out.print("Tipo Sanguíneo: ");
+            String tipoSanguineo = scanner.nextLine();
+
+            List<String> alergias = new ArrayList<>();
+            System.out.print("O paciente possui alergias? (S/N): ");
+            String respostaAlergia = scanner.nextLine().toUpperCase();
+
+            if (respostaAlergia.equalsIgnoreCase("S")) {
+                System.out.print("Digite as alergias (separadas por vírgula): ");
+                String alergiasPaciente = scanner.nextLine();
+                
+                if (alergiasPaciente != null && !alergiasPaciente.trim().isEmpty()) {
+                    
+                    String[] alergiasArray = alergiasPaciente.split(",");
+                    for (String alergia : alergiasArray) {
+                        alergias.add(alergia.trim());
+                    }
+                }
+            }
+
+            List<String> medicamentosEmUso = new ArrayList<>();
+            System.out.print("O paciente utiliza algum medicamento? (S/N): ");
+            String respostaMedicamento = scanner.nextLine().toUpperCase();
+
+            if (respostaMedicamento.equalsIgnoreCase("S")) {
+                System.out.print("Digite os medicamentos (separados por vírgula): ");
+                String medicamentoEmUsoPaciente = scanner.nextLine();
+                
+                if (medicamentoEmUsoPaciente != null && !medicamentoEmUsoPaciente.trim().isEmpty()) {
+                    
+                    String[] medicamentosEmUsoArray = medicamentoEmUsoPaciente.split(",");
+                    for (String medicamentos : medicamentosEmUsoArray) {
+                        medicamentosEmUso.add(medicamentos.trim());
+                    }
+                }
+            }
+
+            Convenio convenio = null;
+            System.out.print("Paciente tem plano de saúde? (S/N): ");
+            String respostaConvenio = scanner.nextLine().toUpperCase();
+
+            if (respostaConvenio.equalsIgnoreCase("S")){
+                System.out.print("Nome do Plano: ");
+                String nomeDoPlano = scanner.nextLine();
+
+                System.out.print("Número do Plano: ");
+                String numeroDoPlano = scanner.nextLine();
+
+                System.out.print("Tipo de Plano: ");
+                String tipoDePlano = scanner.nextLine();
+
+                convenio = new Convenio(nomeDoPlano, numeroDoPlano, tipoDePlano);
+            }     
+            
+            
+            System.out.print("Nome do Contato de Emergência: ");
+            String nomeContatoEmergencia = scanner.nextLine();
+
+            System.out.print("Número do Contato de Emergência: ");
+            String numeroContatoEmergencia = scanner.nextLine();
+
+            System.out.print("Parentesco com o Contato de Emergência: ");
+            String parentescoContatoDeEmergencia = scanner.nextLine();
+
+            try {
+                Paciente novoPaciente = new Paciente(nome, cpf, email, idade, telefone, sexoBiologico, numeroProntuario, status, tipoSanguineo, alergias, medicamentosEmUso, nomeContatoEmergencia, numeroContatoEmergencia, parentescoContatoDeEmergencia, convenio);
+
+                Paciente.cadastrar(novoPaciente);
+                System.out.println("Paciente cadastrado! Número do Prontuário: " + numeroProntuario + "\b");
+            } catch (IllegalArgumentException e) {
+                System.out.println("Erro ao cadastrar paciente: " + e.getMessage());
+
+            }
+        }
     }
-}
+    
+    
+
