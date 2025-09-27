958956f7d69040ed6606771a5934bc9598dcd460
Felipe Melo <felipems.ctt@gmail.com>
2025-09-26 21:44:14 -0300

    Criação de métodos de cadastro e listagem dos pacientes

--- entidades/Paciente.java
+++ entidades/Paciente.java
@@ -1,38 +1,123 @@
 package Projeto1Poo.entidades;
-// import java.util.List;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Paciente extends Pessoa {
+    private static List<Paciente> todosPacientes = new ArrayList<>();
+
     private String numeroProntuario;
-    /*private String dataCadrastro;
-    private String planodesaude;
-
-    private String dataSaida;
-    private String Status;
-    private String historico;
-
+    private String status;
     private String tipoSanguineo;
     private List<String> alergias;
     private List<String> medicamentosEmUso;
 
     private String nomeContatoEmergencia;
     private String numeroContatoEmergencia;
-    private String parentescoContatoDeEmergência;*/
+    private String parentescoContatoDeEmergencia;
+    private Convenio convenio;
 
 
 
 
-    public Paciente(String nome, String cpf, String endereço, String email, int idade, String telefone, char sexoBiologico, String numeroProntuario){
+    public Paciente(String nome, String cpf, String email, int idade, String telefone, String sexoBiologico, String numeroProntuario, String status, String tipoSanguineo, List<String> alergias, List<String> medicamentosEmUso, String nomeContatoEmergencia, String numeroContatoEmergencia, String parentescoContatoDeEmergencia, Convenio convenio) {
 
-        super(nome, cpf, endereço, email, idade, telefone, sexoBiologico);
+        super(nome, cpf, email, idade, telefone, sexoBiologico);
+        this.numeroProntuario = numeroProntuario;
+        this.status = status;
+        this.tipoSanguineo = tipoSanguineo;
+        this.alergias = alergias;
+        this.medicamentosEmUso = medicamentosEmUso;
+        this.nomeContatoEmergencia = nomeContatoEmergencia;
+        this.numeroContatoEmergencia = numeroContatoEmergencia;
+        this.parentescoContatoDeEmergencia = parentescoContatoDeEmergencia;
+        this.convenio = convenio;
+    }
+
+    public static void cadastrar(Paciente paciente) {
+        for (Paciente a : todosPacientes) {
+            if (a.getCpf().equals(paciente.getCpf())) {
+                throw new IllegalArgumentException("Cpf já cadastrado!");
+            }
+        }
+        todosPacientes.add(paciente);
+    }
+
+    public static List<Paciente> listarTodos() {
+        return new ArrayList<>(todosPacientes);
+    }
+
+    public String getNumeroProntuario() {
+        return numeroProntuario;
+    }
+
+    public void setNumeroProntuario(String numeroProntuario) {
         this.numeroProntuario = numeroProntuario;
     }
 
-    public String getNumeroProntuario(){
-        return numeroProntuario;
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTipoSanguineo() {
+        return tipoSanguineo;
+    }
+
+    public void setTipoSanguineo(String tipoSanguineo) {
+        this.tipoSanguineo = tipoSanguineo;
+    }
+
+    public List<String> getAlergias() {
+        return alergias;
+    }
+
+    public void setAlergias(List<String> alergias) {
+        this.alergias = alergias;
+    }
+
+    public List<String> getMedicamentosEmUso() {
+        return medicamentosEmUso;
+    }
+
+    public void setMedicamentosEmUso(List<String> medicamentosEmUso) {
+        this.medicamentosEmUso = medicamentosEmUso;
+    }
+
+    public String getNomeContatoEmergencia() {
+        return nomeContatoEmergencia;
+    }
+
+    public void setNomeContatoEmergencia(String nomeContatoEmergencia) {
+        this.nomeContatoEmergencia = nomeContatoEmergencia;
+    }
+
+    public String getNumeroContatoEmergencia() {
+        return numeroContatoEmergencia;
+    }
+
+    public void setNumeroContatoEmergencia(String numeroContatoEmergencia) {
+        this.numeroContatoEmergencia = numeroContatoEmergencia;
+    }
+
+    public String getParentescoContatoDeEmergencia() {
+        return parentescoContatoDeEmergencia;
+    }
+
+    public void setParentescoContatoDeEmergencia(String parentescoContatoDeEmergencia) {
+        this.parentescoContatoDeEmergencia = parentescoContatoDeEmergencia;
     }
     
-    public void setNumeroPronturario(String numeroProntuario){
-    this.numeroProntuario = numeroProntuario;
-    }  
-    
+    public Convenio getConvenio() {
+        return convenio;
+    }
+
+    public void setConvenio(Convenio convenio) {
+        this.convenio = convenio;
+    }
 }
+
+
+    
