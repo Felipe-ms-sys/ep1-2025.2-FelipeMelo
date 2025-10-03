b826d59a5d21274808eb144eb214ca555ec038e7
Felipe Melo <felipems.ctt@gmail.com>
2025-10-03 17:10:22 -0300

    Criação da classe Internação e seus atributos

---
+++ entidades/Internacao.java
@@ -0,0 +1,11 @@
+package Projeto1Poo.entidades;
+
+public class Internacao {
+    private String dataInternacao;
+    private String dataAlta;
+    private Paciente paciente;
+    private Medico medicoResponsavel;
+    private String status;
+    private int leitosDisponiveis;
+
+}
